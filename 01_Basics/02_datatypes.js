3a74604e54b8f99a4bb4b25376bd933dd5202af6
Harrieee123 <143493179+Harrieee123@users.noreply.github.com>
2024-07-18 15:52:16 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,33 @@
+
+
+
+
+console.log(3
+    +
+     3) // \code readability should be high
+
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 1
+let isloggedin = false 
+
+let state;
+
+/*
+> number => 2 to power 53 (this a no. range in memory address)
+
+> bigint (if the no. is too big it is stored in big int such as reddit, trading, stock market etc.)
+> string => " "
+> boolean => true/false 
+> null => standalone value (meaning empty)
+> undefined => when a variable is declared but no value is assigned
+> Symbol => unique
+
+> object 
+*/
+
+console.log(typeof age); //to know what type it is 
+console.log(typeof null);
+console.log(typeof undefined); //it's type is undefined only 
+
