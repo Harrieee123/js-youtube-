407049ab4cc5d4d8386a2edb2e96c9b7036c9e52
Harrieee123 <143493179+Harrieee123@users.noreply.github.com>
2024-07-18 15:58:31 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 112345
+let accountEmail = "aaditya.pathak2004@gmail.com"
+var accountPassword = "12345"
+accountcity ="jaipur"
+
+//accountId = 2 // it will give error since constant is permanent 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+accountEmail = "shikhaspathak28@gmal.com"
+accountPassword = "6767"
+accountcity = "bengal"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountcity])
